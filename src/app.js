55f6cfd572d91155606f856b0a7665424a9a539d
sachin-malani <sachinmalani32@gmail.com>
2025-01-27 12:02:49 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hi There");
+});
+
+app.listen(3000, () => {
+  console.log("Server up and running 🚀🚀");
+});
