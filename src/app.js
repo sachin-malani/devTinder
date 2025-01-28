8696f0f881a580c6289aee9bc6df9edd2f600b68
sachin-malani <sachinmalani32@gmail.com>
2025-01-28 12:41:46 +0530

    User model and signup api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const { connect } = require("./db/database");
+const User = require("./models/user");
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hi There");
 });
 
-app.listen(3000, () => {
-  console.log("Server up and running 🚀🚀");
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    const result = await user.save();
+    res.send(result);
+  } catch (error) {
+    res.send({ err: error });
+  }
 });
+
+connect()
+  .then(() => {
+    console.log("Database Connected Successfully 🚀🚀🚀");
+    app.listen(3000, () => {
+      console.log("Server up and running 🚀🚀");
+    });
+  })
+  .catch((err) => {
+    console.error("Error while connecting to database : ", err);
+  });
