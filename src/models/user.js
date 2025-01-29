3cc2ee2327f3e51d9e3372ec8111a7d9ebc6d188
sachin-malani <sachinmalani32@gmail.com>
2025-01-29 14:26:46 +0530

    user schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,56 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  age: Number,
-  gender: String,
-  email: String,
-  password: String,
-});
+const userSchema = mongoose.Schema(
+  {
+    firstName: { type: String, required: true, minLength: 1, maxLength: 30 },
+    lastName: { type: String, minLength: 1, maxLength: 30 },
+    age: { type: Number, required: true, min: 14 },
+    gender: {
+      type: String,
+      enum: {
+        values: ["M", "F", "O"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: {
+        validator: function (value) {
+          return validator.isEmail(value);
+        },
+        message: "Please enter a valid email address",
+      },
+    },
+    password: { type: String, required: true },
+    skills: {
+      type: [String],
+      validate: {
+        validator: function (skills) {
+          return skills.length <= 10;
+        },
+        message: "User can add upto 10 skills",
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://img.icons8.com/fluency-systems-regular/50/user--v1.png",
+      validate: {
+        validator: function (value) {
+          return validator.isURL(value);
+        },
+        message: "Please Enter a valid URL",
+      },
+    },
+    about: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
