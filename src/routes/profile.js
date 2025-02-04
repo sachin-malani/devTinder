3a34229ef847b76a306243f041c4e8f7c9a0582e
sachin-malani <sachinmalani32@gmail.com>
2025-02-04 18:03:21 +0530

    forgot password api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,10 +2,12 @@ const express = require("express");
 const route = express.Router();
 const { userAuth } = require("../middleware/auth");
 const User = require("../models/user");
+const { isValidUpdateFields } = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
 route.get("/", userAuth, async (req, res) => {
   try {
-    const user = await User.findById(req.id);
+    const user = await User.findById(req.id, "-password -createdAt -updatedAt");
 
     if (!user) throw new Error("Invalid User");
 
@@ -15,22 +17,8 @@ route.get("/", userAuth, async (req, res) => {
   }
 });
 
-route.get("/user", userAuth, async (req, res) => {
-  const body = req.body;
-  try {
-    const user = await User.findOne({ email: body.email });
-    if (!user) {
-      res.status(404).send("User not found");
-    } else {
-      res.send(user);
-    }
-  } catch (error) {
-    res.status(500).send({ err: error });
-  }
-});
-
-route.delete("/user", userAuth, async (req, res) => {
-  const id = req.body.userId;
+route.delete("/remove", userAuth, async (req, res) => {
+  const id = req.id;
   try {
     const result = await User.findByIdAndDelete(id);
     res.send(result);
@@ -39,23 +27,43 @@ route.delete("/user", userAuth, async (req, res) => {
   }
 });
 
-route.patch("/user", userAuth, async (req, res) => {
-  const id = req.body.userId;
+route.patch("/update", userAuth, async (req, res) => {
   try {
-    const ALLOWED_UPDATES = ["userId", "skills", "about", "photoUrl"];
-    const isUpdateAllowed = Object.keys(req.body).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-
-    if (!isUpdateAllowed)
+    if (!isValidUpdateFields(req))
       return res.send({ err: "Invalid Input, update denied" });
 
-    const result = await User.findByIdAndUpdate(id, req.body, {
+    //No Validation
+    // const user = await User.findOne(id);
+    // Object.keys(req.body).forEach((key) => (user[key] = req.body[key]));
+    // user.save();
+
+    const result = await User.findByIdAndUpdate(req.id, req.body, {
       runValidators: true,
-    });
-    res.send(result);
+      returnDocument: "after",
+    }).select("firstName lastName age gender email skills about photoUrl");
+
+    res.json({ message: "Updated Successfully", result });
   } catch (error) {
     res.status(500).send({ err: error.message });
+  }
+});
+
+route.patch("/password", userAuth, async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  try {
+    const user = await User.findById(req.id, "password");
+    if (!user) throw new Error("Invalid Token");
+
+    const isValidPassword = await user.verifyPassword(oldPassword);
+    if (!isValidPassword) throw new Error("Invalid credentials ");
+
+    const newHashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = newHashedPassword;
+    user.save();
+
+    res.json({ message: "Password Updated Successfully" });
+  } catch (error) {
+    res.status(500).json({ err: error.message });
   }
 });
 
