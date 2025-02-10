0267f045d423d32f634d1b0c8d3a5f471203684e
sachin-malani <sachinmalani32@gmail.com>
2025-02-10 17:41:53 +0530

    review request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
-const connectionRequest = require("../models/connectionRequest");
-const { isValidStatus } = require("../utils/validation");
+const ConnectionRequest = require("../models/connectionRequest");
+const {
+  isValidRequestStatus,
+  isValidReviewStatus,
+} = require("../utils/validation");
 const User = require("../models/user");
 const route = express.Router();
 
@@ -11,13 +14,13 @@ route.post("/:status/:toUserId", userAuth, async (req, res) => {
     const toUserId = req.params.toUserId;
     const status = req.params.status;
 
-    if (!isValidStatus(status))
+    if (!isValidRequestStatus(status))
       return res.status(400).json({ message: "Invalid Status - " + status });
 
     const toUser = await User.findById(toUserId);
     if (!toUser) return res.status(404).json({ message: "Invalid User" });
 
-    const isExistingConnection = await connectionRequest.findOne({
+    const isExistingConnection = await ConnectionRequest.findOne({
       $or: [
         { fromUserId, toUserId },
         { fromUserId: toUserId, toUserId: fromUserId },
@@ -26,7 +29,7 @@ route.post("/:status/:toUserId", userAuth, async (req, res) => {
     if (isExistingConnection)
       return res.json({ message: "Connection request already exists" });
 
-    const newConnectionRequest = new connectionRequest({
+    const newConnectionRequest = new ConnectionRequest({
       fromUserId,
       toUserId,
       status,
@@ -34,6 +37,32 @@ route.post("/:status/:toUserId", userAuth, async (req, res) => {
 
     await newConnectionRequest.save();
     res.json({ message: `Connection Request sent to ${toUser.firstName}` });
+  } catch (error) {
+    res.status(500).json({ err: error.message });
+  }
+});
+
+route.post("/review/:status/:requestId", userAuth, async (req, res) => {
+  try {
+    const userId = req.id;
+    const status = req.params.status;
+
+    if (!isValidReviewStatus(status))
+      return res.status(400).json({ message: "Invalid Status - " + status });
+
+    const isConnectionRequest = await ConnectionRequest.findOne({
+      _id: req.params.requestId,
+      toUserId: userId,
+      status: "interested",
+    });
+
+    if (!isConnectionRequest)
+      return res.status(404).json({ message: "Connection request not found" });
+
+    isConnectionRequest.status = status;
+    await isConnectionRequest.save();
+
+    res.json({ message: "Connection request - " + status.toUpperCase() });
   } catch (error) {
     res.status(500).json({ err: error.message });
   }
