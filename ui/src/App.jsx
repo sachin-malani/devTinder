051705b4ca53059cabc80a1a4dd81e8541f3f53e
sachin-malani <sachinmalani32@gmail.com>
2025-02-13 11:40:56 +0530

    routing

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
 function App() {
   return (
     <>
-      <h1>Dev Tinder</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
